8e2565364d9d9f7467d9fc928385c8225064eef4
Illia Andreluka <wandershlepa@gmail.com>
2025-03-11 20:32:46 +0100

    Added code.

--- js/script.js
+++ js/script.js
@@ -1,71 +1,120 @@
-/*
- ! Завдання: Реалізація Функції з Каруванням для Послідовного Додавання Трьох Чисел
-*/
-
-function curriedAdd(a) {
-    return function(b) {
-      return function(c) {
-        return a + b + c;
-      }
-    }
-  }
-  
-  console.log(curriedAdd(1)(2)(3));
-
-
-
+console.log('#5. JavaScript homework example file')
 
 /*
- ! Завдання: Реалізація Функції з Каруванням для Побудови Повного Доменного Імені
-*/
+ * #1
+ *
+ * Створіть функцію counter(), яка має реалізувати лічильник за допомогою замикання:
+ * функція може приймати число як аргумент counter(n)
+ * якщо число передано у функцію - лічба починається із зазначеного числа
+ * якщо ні - то лічба триває
+ */
 
-function curriedDomain(protocol) {
-    return function protocolSetter(domainName) {
-        return function domainNameSetter(tld) {
-            return protocol + "://" + domainName + "." + tld;
-        }
-    }
-  }
+const counter = function(n) {
+        let count = 0
+        return n !== undefined ? count++ : count + n
+}
 
-  const protocolSetter = curriedDomain('https')
-  const domainNameSetter = protocolSetter('github')
-  const fullDomain = domainNameSetter('com')
-  console.log('Full Domain:', fullDomain)
+// const counter = function() {}
 
+console.log(counter()) // 0
+console.log(counter()) // 1
+console.log(counter(100)) // 100
+console.log(counter()) // 101
+console.log(counter()) // 102
+console.log(counter(500)) // 500
+console.log(counter()) // 501
+console.log(counter()) // 502
+console.log(counter(0)) // 0
+console.log(counter()) // 0
+console.log(counter()) // 1
+
+/*
+ * #2
+ *
+ * Створіть функцію counterFactory, яка має реалізувати три методи за допомогою замикання:
+ * початкове значення лічильника - 0
+ * counterFactory.value() - повертає значення лічильника
+ * counterFactory.value(n) - встановлює значення лічильника, повертає нове значення
+ * counterFactory.increment() - збільшує значення лічильника на 1
+ * counterFactory.decrement() - зменшує значення лічильника на 1
+ */
+
+// const counterFactory = function () {}
+
+// console.log(counterFactory.value()) // 0
+// counterFactory.increment()
+// counterFactory.increment()
+// counterFactory.increment()
+// console.log(counterFactory.value()) // 3
+// counterFactory.decrement()
+// counterFactory.decrement()
+// console.log(counterFactory.value()) // 1
+// console.log(counterFactory.value(100)) // 100
+// counterFactory.decrement()
+// console.log(counterFactory.value()) // 99
+// console.log(counterFactory.value(200)) // 200
+// counterFactory.increment()
+// console.log(counterFactory.value()) // 201
+
+/*
+ * #3
+ *
+ * Створіть функцію myPow(a, b, myPrint). Всередині реалізуйте рекурсію для підрахунку результату піднесення числа a до ступеня b.
+ * - Функція myPrint(a, b, res) - глобальна функція, що має генерувати з параметрів a, b, res рядок вигляду 'a^b=res' і повертати його.
+ * - myPrint() має бути передана в myPow() як параметр і викликана всередині як callback-функція.
+ * - функція myPow() як значення, що повертається, приймає результат myPrint().
+ * Наприклад:
+ * console.log(myPow(3, 4, myPrint)); // 3^4=81
+ * console.log(myPow(2, 3, myPrint)); // 2^3=8
+ * console.log(myPow(2, 0, myPrint))  // 2^0=1
+ * console.log(myPow(2, -2, myPrint)) // 2^-2=0.25
+ */
+
+// const myPrint = () => {}
+// const myPow = () => {}
+
+// console.log(myPow(3, 4, myPrint)) // 3^4=81
+// console.log(myPow(2, 3, myPrint)) // 2^3=8
+// console.log(myPow(2, 0, myPrint)) // 2^0=1
+// console.log(myPow(2, -2, myPrint)) // 2^-2=0.25
 
 
 /*
- ! Завдання: Розробка Вищої Функції для Модифікації Поведінки Іншої Функції
-*/
+ * #4
+ * Створіть функцію myMax(arr), яка як параметр приймає
+ * довільний числовий масив і повертає максимальне число з переданого їй масиву.
+ * У реалізації функції має бути застосовано метод Math.max() і apply().
+ */
 
-function originalFunction(num) {
-    return num * num
-  }
-  
-  function modifyFunction(originalFunc, multiplier) {
-    return function(num) {
-      return originalFunc(num) * multiplier
-    }
-  }
+// const list = [12, 23, 100, 34, 56, 9, 233]
+// const myMax = () => {}
 
-  const modifiedFunc = modifyFunction(originalFunction, 3)
-  console.log('Original function output for 4:', originalFunction(4))
-  console.log('Modified function output for 4:', modifiedFunc(4))
-  
-
+// console.log(myMax(list)); // 233
 
 /*
-! Завдання: Розробка Функції Трирівневої Вкладеності в JavaScript з Конкретною Операцією
-*/
+ * #5
+ *
+ * Створіть функцію myMul(a, b), яка буде множити числа а і b, повертаючи результат.
+ */
 
-function outerFunction(arg1) {
-    return function innerFunction(arg2) {
-      return function deepInnerFunction(arg3) {
-        return arg1 * arg2 * arg3;
-      }
-    }
-  }
-  
-  const result = outerFunction(2)(3)(4)
-  console.log(result)
-  
+// const myMul = () => {}
+
+/*
+ * Створіть функції myDouble(n), яка приймає один параметр і подвоює його.
+ * Використовувати множення або інші математичні операції всередині функції - заборонено, тільки bind() і myMul().
+ * Функція повертає результат обчислення.
+ */
+
+// const myDouble
+
+// console.log(myDouble(3)) // = myMul(2, 3) = 6
+// console.log(myDouble(4)) // = myMul(2, 4) = 8
+// console.log(myDouble(5)) // = myMul(2, 5) = 10
+
+// Аналогічним чином створюємо функцію myTriple(n), яка потроює параметр, що приймає, повертаючи результат.
+
+// const myTriple
+
+// console.log(myTriple(3)) // = myMul(3, 3) = 9
+// console.log(myTriple(4)) // = myMul(3, 4) = 12
+// console.log(myTriple(5)) // = myMul(3, 5) = 15
